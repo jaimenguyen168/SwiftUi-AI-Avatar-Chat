2aace8be1f69819cad2dc4f7a8061d0f9bcdf16a
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2024-12-19 12:24:40 -0500

    Initial Commit

---
+++ AI-Avatar-Chat/AI_Avatar_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Avatar_ChatApp.swift
+//  AI-Avatar-Chat
+//
+//  Created by Dat Nguyen on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AI_Avatar_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI-Avatar-ChatTests/AI_Avatar_ChatTests.swift
@@ -0,0 +1,16 @@
+//
+//  AI_Avatar_ChatTests.swift
+//  AI-Avatar-ChatTests
+//
+//  Created by Dat Nguyen on 12/19/24.
+//
+
+import Testing
+
+struct AI_Avatar_ChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
