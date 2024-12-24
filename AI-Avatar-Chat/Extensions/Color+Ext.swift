c11cf1f0060cc4342e0b023366560c29f1f22247
Jaime <nguyenphuocdat168@gmail.com>
2024-12-24 00:13:37 -0500

    [Feature] User Model and User Profile

---
+++ AI-Avatar-Chat/Extensions/Color+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Ext.swift
+//  AI-Avatar-Chat
+//
+//  Created by Dat Nguyen on 12/23/24.
+//
+
+import Foundation
---
+++ AI-Avatar-Chat/Services/User/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  AI-Avatar-Chat
+//
+//  Created by Dat Nguyen on 12/23/24.
+//
+
+import Foundation
